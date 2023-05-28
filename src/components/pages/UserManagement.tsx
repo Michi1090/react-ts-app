88afc3db2d4f6203f75701eb13a8911c1fd84906
Michi <michi.m1090@gmail.com>
2023-05-28 11:17:57 +0900

    ADD:ユーザー一覧画面

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,33 @@
-import { memo, VFC } from "react";
+import { memo, useEffect, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://picsum.photos/260"
+                userName={user.username}
+                fullName={user.name}
+              ></UserCard>
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
